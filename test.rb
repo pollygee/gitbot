d89dcba77b41adf50c192b5ed369f10db6b790f6
Polly Sileo <pollysileo@gmail.com>
2015-06-10 15:17:48 -0400

    One test up

---
+++ test.rb
@@ -0,0 +1,45 @@
+require 'minitest/autorun'
+require 'rack/test'
+
+ENV["TEST"] = ENV["RACK_ENV"] = "test"
+
+require './db/setup'
+require './lib/all'
+
+#require './server'
+
+require 'pry'
+
+class GifBotTest < Minitest::Test
+  include Rack::Test::Methods
+  def app
+    #GifBotWeb
+  end
+
+  def setup
+    Gif.delete_all
+    Tag.delete_all
+    GifTag.delete_all
+  end
+
+  def test_users_can_add_gifs
+    post "add",
+      url: "www.google.com",
+      name: "Mark"
+    assert_equal 200, last_response.status
+
+    gif = Gif.find_by_url "www.google.com"
+    assert gif
+    assert_equal gif.id.to_s, last_response.body
+  end
+
+  def test_url_can_not_be_added_wihtout_a_name
+
+  end
+
+  def test_can_not_add_without_a_url
+
+  end
+
+
+end
