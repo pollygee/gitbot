8018b5b7443eb80476eea8091fb9d5bbd325ef1b
MarkHyland <markdhyland@gmail.com>
2015-06-10 15:29:38 -0400

    change

--- server.rb
+++ server.rb
@@ -7,9 +7,8 @@ class GifbotWeb < Sinatra::Base
   post "/add" do 
     list = GifbotWeb.new
     gif = list.add_gif params[:username], params[:url]
-    gif.username.to_s
+    gif.id.to_s
   end
-
 
 
 end
@@ -21,22 +20,22 @@ end
 
 
 
-class ToDoWeb < Sinatra::Base
-  set :logging, true
+# class ToDoWeb < Sinatra::Base
+#   set :logging, true
 
-  post "/add" do
-    listicize = ToDoList.new
-    item = listicize.create_entry params[:list], params[:description], params[:due_date]
-    item.id.to_s
-  end
+#   post "/add" do
+#     listicize = ToDoList.new
+#     item = listicize.create_entry params[:list], params[:description], params[:due_date]
+#     item.id.to_s
+#   end
 
-  get "/list/:name" do
-    list = List.find_by_list_name! params[:name]
-    list.items.to_json
-  end
-end
+#   get "/list/:name" do
+#     list = List.find_by_list_name! params[:name]
+#     list.items.to_json
+#   end
+# end
 
-# $0 is $PROGRAM_NAME
-if $0 == __FILE__
-  ToDoWeb.start!
-end
+# # $0 is $PROGRAM_NAME
+# if $0 == __FILE__
+#   ToDoWeb.start!
+# end
